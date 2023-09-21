a1fa0b3a79c924516d0792314badde769869d803
bigotes invisibles <matiasriscolagos@gmail.com>
2023-09-21 21:32:28 +0200

    Add function setName

--- js/index.js
+++ js/index.js
@@ -5,22 +5,32 @@ const pictureInputElement = formElement.querySelector("#picture");
 const javascriptInputElement = formElement.querySelector("#javascript");
 const sufferingInputElement = formElement.querySelector("#suffering");
 
+const setName = (name) => {
+  document.querySelector(".name").textContent = name;
+  document.querySelector(".greeting.off").className = "greeting on";
+};
+
 formElement.addEventListener("change", (event) => {
   switch (event.target) {
     case nameInputElement:
       // Invoca a la función para poner el nombre
+      setName(event.target.value);
       break;
     case birthdateInputElement:
       // Invoca a la función para poner la edad
+
       break;
     case pictureInputElement:
       // Invoca a la función para poner la imagen
+
       break;
     case javascriptInputElement:
       // Invoca a la función para poner si ha programado previamente JS o no
+
       break;
     case sufferingInputElement:
       // Invoca a la función para poner el grado de sufrimiento
+
       break;
   }
 });
